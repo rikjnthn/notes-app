e5b57c136d242179cfd093885409f98ff94e8b37
Erik <rikr9879@gmail.com>
2022-12-23 19:56:48 +0700

    ana

--- src/Components/SideNav/FolderProps/FileProps/index.tsx
+++ src/Components/SideNav/FolderProps/FileProps/index.tsx
@@ -22,7 +22,7 @@ const FileProps = ({
   const { FileID } = useParams();
 
   const onDelete = () => {
-    if (id === FileID) location.href = `http://${location.host}/notes-app`;
+    if (id === FileID) location.href = `http://${location.host}/notes-app/`;
     
     setFolderState((prevState) => {
       return {
