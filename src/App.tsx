b09a4e1c33fe22df1dadeac9d18fe78a1209e9cd
Erik <rikr9879@gmail.com>
2022-12-24 14:17:35 +0700

    akgasjkgajkghnk

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <ChakraProvider>
       <Suspense fallback={<Loading />}>
-        <Router basename="notes-app">
+        <Router>
           <Routes>
             <Route path="/" element={<SideNav />}>
               <Route path=":FolderName">
